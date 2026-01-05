b545bfa2847f81d90e804a92793fb90ca413cebd
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-05 14:29:48 +0100

    feat: initialize server application with Express setup, logging, and environment configuration.

---
+++ src/common/utils/logger.ts
@@ -0,0 +1,46 @@
+import { createLogger, transports, format } from 'winston';
+import path from 'path';
+
+const logFormat = format.combine(
+    format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss'
+    }),
+    format.label({
+        label: 'right meow!'
+    }),
+    format.json(),
+    format.splat(),
+    format.errors({
+        stack: true
+    }),
+    format.colorize(),
+    format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+);
+
+const logger = createLogger({
+    level: 'info',
+    format: logFormat,
+    transports: [
+        new transports.File({
+            filename: path.join(__dirname, 'logs', 'error.log'),
+            level: 'error'
+        }),
+        new transports.File({
+            filename: path.join(__dirname, 'logs', 'combined.log')
+        })
+    ]
+});
+
+if (process.env.NODE_ENV === 'development') {
+    logger.add(
+        new transports.Console({
+            format: format.combine(
+                format.colorize(),
+                format.simple(),
+                format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+            )
+        })
+    );
+}
+
+export { logger };
