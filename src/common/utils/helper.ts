708d2ecc3e8f3c08a1597366f70829c5861b9fbe
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-05 14:28:32 +0100

    feat: Initialize core server utilities, environment configuration, and Express application setup.

---
+++ src/common/utils/helper.ts
@@ -0,0 +1,6 @@
+const oneHour = 60 * 60 * 1000;
+const fiveMinutes = 5 * 60 * 1000;
+const fifteenMinutes = 15 * 60 * 1000;
+const twentyFourHours = 24 * 60 * 60 * 1000;
+
+export { oneHour, fiveMinutes, fifteenMinutes, twentyFourHours };
