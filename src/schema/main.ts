8358cab9bd2d9c62e8e29b41f9e0041d3ef28b04
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-06 08:54:17 +0100

    feat: implement Zod schemas and middleware for request body validation.

---
+++ src/schema/main.ts
@@ -0,0 +1,40 @@
+import { dateFromString } from '@/common';
+
+import * as z from 'zod';
+
+const passwordRegexMessage =
+      'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character';
+
+const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+export const mainSchema = z
+      .object({
+            username: z
+                  .string()
+                  .min(3, 'username must be at least 3 characters long')
+                  .max(15, 'username must be at most 15 characters long')
+                  .regex(/^[a-zA-Z0-9_]+$/, { message: 'username must contain only letters, numbers and underscores' }),
+            email: z.string().email('Invalid email address'),
+            password: z
+                  .string()
+                  .min(8, 'password must be at least 8 characters long')
+                  .regex(passwordRegex, { message: passwordRegexMessage }),
+            confirmPassword: z
+                  .string()
+                  .min(8, 'confirm password must be at least 8 characters long')
+                  .regex(passwordRegex, { message: passwordRegexMessage }),
+            code: z
+                  .string()
+                  .min(6, 'code must be at least 6 characters long')
+                  .max(6, 'code must be at most 6 characters long'),
+            token: z.string(),
+            userId: z.string().regex(/^[0-9a-fA-F]{24}$/, { message: 'Invalid user ID' }),
+            isTermsAndConditionAcepted: z.boolean(),
+            redirectUrl: z.string().url()
+      })
+      .refine(data => data.password === data.confirmPassword, {
+            message: 'passwords do not match',
+            path: ['confirmPassword'] // This ensures the error is attached to the confirmPassword field
+      });
+
+export const partialMainSchema = mainSchema.partial();
