aa46de7e4749521a5d711246a7cfbf97330897d4
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-08 12:27:37 +0100

    feat: added ade job functionality

--- src/common/utils/queue.ts
+++ src/common/utils/queue.ts
@@ -2,3 +2,22 @@ import { Job } from 'bullmq';
 
 import { logger } from './logger';
 import { addJob, createWorker, getJobStatus, clearQueue, closeQueueConnections, sendEmail } from '@/config';
+import { EmailJobData as EmailJobDataType } from '@/common';
+
+// queue utility functions
+const queueUtils = {
+        /**
+         * Add a job to a queue
+         * @param queueName - The name of the queue
+         * @param jobData - The job data
+         * @param options - Job options (optional)
+         */
+
+        async addJob(
+                queueName: string,
+                jobData: EmailJobDataType,
+                options?: { delay?: number; priority?: number; attempts?: number; jobId?: string }
+        ): Promise<string> {
+                return addJob(queueName, jobData, options);
+        }
+};
