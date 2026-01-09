c5f85925867991eefb2341912768dd235422ccc0
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-09 08:48:11 +0100

    feat: created function to sanitize request ody

--- src/common/utils/helper.ts
+++ src/common/utils/helper.ts
@@ -1,36 +1,38 @@
 const dateFromString = async (value: string) => {
-      const date = new Date(value);
+        const date = new Date(value);
 
-      if (isNaN(date?.getTime())) {
-            throw new Error('Invalid date');
-      }
+        if (isNaN(date?.getTime())) {
+                throw new Error('Invalid date');
+        }
 
-      return date;
+        return date;
 };
 
-const sanitizeRequestBody = (data: Record<string, any>) => {
-      const sanitize: Record<string, any> = {};
+const sanitizeRequestBody = (data: Record<string, unknown>) => {
+        const sanitize: Record<string, unknown> = {};
 
-      for (const [key, value] of Object.entries(data)) {
-            if (value === null || value === undefined) {
-                  sanitize[key] = value;
-                  continue;
-            }
+        for (const [key, value] of Object.entries(data)) {
+                if (value === null || value === undefined) {
+                        sanitize[key] = value;
+                        continue;
+                }
 
-            if (typeof value === 'string') {
-                  sanitize[key] = value.trim() === '' ? null : value.trim();
-            } else if (typeof value === 'object' && !Array.isArray(value)) {
-                  sanitize[key] = sanitizeRequestBody(value);
-            } else if (Array.isArray(value)) {
-                  sanitize[key] = value.map(item =>
-                        typeof item === 'object' && item !== null ? sanitizeRequestBody(item) : item
-                  );
-            } else {
-                  sanitize[key] = value;
-            }
-      }
+                if (typeof value === 'string') {
+                        sanitize[key] = value.trim() === '' ? null : value.trim();
+                } else if (typeof value === 'object' && !Array.isArray(value)) {
+                        sanitize[key] = sanitizeRequestBody(value as Record<string, unknown>);
+                } else if (Array.isArray(value)) {
+                        sanitize[key] = value.map(item =>
+                                typeof item === 'object' && item !== null
+                                        ? sanitizeRequestBody(item as Record<string, unknown>)
+                                        : item
+                        );
+                } else {
+                        sanitize[key] = value;
+                }
+        }
 
-      return sanitize;
+        return sanitize;
 };
 
 const oneHour = 60 * 60 * 1000;
