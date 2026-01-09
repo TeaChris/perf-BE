80a5f948a6eb22a262d2d161affc1c5d4f128033
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-09 08:47:15 +0100

    feat: added job data

--- src/common/interface/email.queue.ts
+++ src/common/interface/email.queue.ts
@@ -1,12 +1,16 @@
 export interface CommonDataFields {
-      to: string;
-      priority?: string;
-      username?: string;
+        to: string;
+        priority?: string;
+        username?: string;
 }
 
 export interface WelcomeEmailData extends CommonDataFields {
-      verificationLink: string;
-      email: string;
+        verificationLink: string;
+        email: string;
 }
 
-export type EmailJobData = WelcomeEmailData;
+export type EmailJobData = { type: 'welcomeEmail'; data: WelcomeEmailData };
+//   | { type: 'resetPassword'; data: ResetPasswordData }
+//   | { type: 'forgotPassword'; data: ForgotPasswordData }
+//   | { type: 'restoreAccount'; data: RestoreAccountData }
+//   | { type: 'fallbackOTP'; data: FallbackOTPEmailData };
