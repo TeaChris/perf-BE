3afbb0f2a53a93d04a87c92200c5dd2f87224c6d
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-26 15:24:45 +0100

    feat: Implement Zod-based request body validation middleware and integrate it into the Express application.

--- src/middleware/validate.with.zod.ts
+++ src/middleware/validate.with.zod.ts
@@ -2,38 +2,38 @@ import * as z from 'zod';
 import { NextFunction, Request, Response } from 'express';
 
 import { catchAsync } from './catch.async';
-import { mainSchema, partialMainSchema } from '@/schema';
 import { sanitizeRequestBody } from '@/common';
 import AppError from '@/common/utils/app.error';
+import { mainSchema, partialMainSchema } from '@/schema';
 
 type MyDataShape = z.infer<typeof mainSchema>;
 
 const methodsToSkipValidation = ['GET'];
 
 const validateDataWithZod = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-      // skip validation for defined methods and routes
-      if (methodsToSkipValidation.includes(req.method)) {
-            return next();
-      }
+        // skip validation for defined methods and routes
+        if (methodsToSkipValidation.includes(req.method)) {
+                return next();
+        }
 
-      const rawData = req.body as Partial<MyDataShape>;
+        const rawData = req.body as Partial<MyDataShape>;
 
-      if (!rawData) return next();
+        if (!rawData) return next();
 
-      // Sanitize input data first
-      const sanitizedData = sanitizeRequestBody(rawData) as Partial<MyDataShape>;
+        // Sanitize input data first
+        const sanitizedData = sanitizeRequestBody(rawData) as Partial<MyDataShape>;
 
-      // Validate only if it contains the fields in req.body against the mainSchema
-      const mainResult = partialMainSchema.safeParse(sanitizedData);
-      if (!mainResult.success) {
-            const errorDetails = mainResult.error;
-            throw new AppError('Validation failed', 422, errorDetails);
-      } else {
-            // this ensures that only fields defined in the mainSchema are passed to the req.body
-            req.body = mainResult.data as Partial<MyDataShape>;
-      }
+        // Validate only if it contains the fields in req.body against the mainSchema
+        const mainResult = partialMainSchema.safeParse(sanitizedData);
+        if (!mainResult.success) {
+                const errorDetails = mainResult.error;
+                throw new AppError('Validation failed', 422, errorDetails);
+        } else {
+                // this ensures that only fields defined in the mainSchema are passed to the req.body
+                req.body = mainResult.data as Partial<MyDataShape>;
+        }
 
-      next();
+        next();
 });
 
 export { validateDataWithZod };
