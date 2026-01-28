a72b8144aba11d9f5225b01a42337c778d0558a9
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-01-28 12:25:10 +0100

    feat: Implement initial server setup including authentication routes, Zod validation, and security middleware.

---
+++ src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { signUp, signIn, signOut } from '@/controller';
+import { registerSchema, loginSchema } from '@/schema';
+import { validateDataWithZod, protect } from '@/middleware';
+
+const router = Router();
+
+router.post('/register', validateDataWithZod(registerSchema), signUp);
+router.post('/login', validateDataWithZod(loginSchema), signIn);
+router.post('/logout', protect, signOut);
+
+export { router as authRouter };
