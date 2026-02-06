fa95723761cea756a0e08a268d0ecfd8ebd06c3c
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-06 09:59:26 +0100

    feat: Add middleware to generate and manage correlation IDs for requests.

--- src/middleware/correlation.ts
+++ src/middleware/correlation.ts
@@ -1,9 +1,9 @@
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 import { context } from '@/common';
 import { Request, Response, NextFunction } from 'express';
 
 export const correlationIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
-        const correlationId = (req.headers['x-correlation-id'] as string) || uuidv4();
+        const correlationId = (req.headers['x-correlation-id'] as string) || randomUUID();
 
         // Set the correlation ID in the response headers
         res.setHeader('X-Correlation-ID', correlationId);
