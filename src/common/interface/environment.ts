3abebb8c3bf4884a1768c29c26e4ad70eece6551
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-10 08:02:13 +0100

    feat: Implement user authentication routes for sign-up, email verification, and sign-in, along with environment configuration and helper utilities.

--- src/common/interface/environment.ts
+++ src/common/interface/environment.ts
@@ -1,33 +1,36 @@
 export interface IEnvironment {
-      APP: {
-            NAME?: string;
-            PORT: number;
-            ENV?: string;
-            CLIENT: string;
-      };
-      DB: {
-            URL: string;
-      };
-      REDIS: {
-            URL: string;
-            PORT: number;
-            PASSWORD: string;
-      };
-      CACHE_REDIS: {
-            URL: string;
-      };
-      JWT: {
-            ACCESS_KEY: string;
-            REFRESH_KEY: string;
-      };
-      JWT_EXPIRES_IN: {
-            REFRESH_SECONDS: number;
-            REFRESH: string;
-            ACCESS: string;
-      };
-      FRONTEND_URL: string;
-      EMAIL: {
-            API_KEY: string;
-            FROM_EMAIL: string;
-      };
+        APP: {
+                NAME?: string;
+                PORT: number;
+                ENV?: string;
+                CLIENT: string;
+        };
+        DB: {
+                URL: string;
+        };
+        REDIS: {
+                URL: string;
+                PORT: number;
+                PASSWORD: string;
+        };
+        CACHE_REDIS: {
+                URL: string;
+        };
+        JWT: {
+                ACCESS_KEY: string;
+                REFRESH_KEY: string;
+        };
+        JWT_EXPIRES_IN: {
+                REFRESH_SECONDS: number;
+                REFRESH: string;
+                ACCESS: string;
+        };
+        FRONTEND_URL: string;
+        EMAIL: {
+                API_KEY: string;
+                FROM_EMAIL: string;
+        };
+        COOKIE: {
+                DOMAIN: string;
+        };
 }
