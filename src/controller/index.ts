ffc0349f78c4eb3a233b3785cf55c910b4fe2b74
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-11 13:33:56 +0100

    feat: Initialize Express server with core API routes, controllers, models, and middleware.

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,2 +1,5 @@
 export * from './error.controller';
 export * from './auth.controller';
+export * from './product.controller';
+export * from './flash-sale.controller';
+export * from './admin.controller';
