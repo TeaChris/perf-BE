2aa9bdc11678454897b118a4fe4e21d757601abb
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-11 13:34:24 +0100

    feat: Implement initial server setup with Express, product, flash sale, and admin routes and models.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,5 +3,6 @@ export * from './correlation';
 export * from './catch.async';
 export * from './timeout';
 export * from './protect';
+export * from './authorize';
 export * from './csrf';
 export * from './sanitizer';
