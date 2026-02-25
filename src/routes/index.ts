e1b56f3130a229bb7761a825094fd1dce34e23b4
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-25 09:49:30 +0100

    feat: implement payment processing with Paystack integration and dedicated routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
-export * from './auth.routes';
-export * from './product.routes';
 export * from './flash-sale.routes';
+export * from './product.routes';
+export * from './payment.routes';
 export * from './admin.routes';
+export * from './auth.routes';
