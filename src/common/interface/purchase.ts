30080171102d3ab7894f6e7c50240c39914fa0b7
bermuda <olasunkanmiboluwatife110@gmail.com>
2026-02-25 09:05:38 +0100

    feat: Implement Paystack payment gateway with webhook handling, purchase management, and real-time updates.

--- src/common/interface/purchase.ts
+++ src/common/interface/purchase.ts
@@ -6,7 +6,10 @@ export interface IPurchase {
         productId: mongoose.Types.ObjectId;
         flashSaleId: mongoose.Types.ObjectId;
         price: number;
-        purchasedAt: Date;
+        status: 'pending' | 'completed' | 'failed' | 'expired';
+        paymentReference: string;
+        expiresAt: Date;
+        purchasedAt?: Date;
         createdAt?: Date;
         updatedAt?: Date;
 }
